25e9d094b3dde8dced87e3673611b4466022ee29
CassiusN <cassius.cr.naylor@gmail.com>
2023-01-10 12:38:34 +0000

    Step 8: Directory Exercise 1

---
+++ directoryexercises.rb
@@ -0,0 +1,52 @@
+# student names in array
+students = [
+  {name: "Dr. Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddy Krueger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffrey Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november}
+]
+
+def input_students
+  puts "Please enter the names of the students (hit return twice to finish)"
+  students = []
+  name = gets.chomp
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print(students)
+  if students[:name[0]] == "A"
+  students.each_with_index do |student, index| 
+  puts " #{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  end
+  else nil
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
+
+
+
