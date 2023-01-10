88845db0ad832fcdbc3b1896966fe77b425c53a0
CassiusN <cassius.cr.naylor@gmail.com>
2023-01-10 14:05:41 +0000

    Step 8: Directory Exercise 3

--- directoryexercises.rb
+++ directoryexercises.rb
@@ -32,7 +32,7 @@ end
 
 def print(students)
   students.each_with_index() do |student, index|
-  unless student[:name][0] == "A"
+  unless student[:name].length > 12 
   puts " #{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
   end
