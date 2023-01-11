6d91c2788e66bcd5819860d913aa7bb523499dfc
CassiusN <cassius.cr.naylor@gmail.com>
2023-01-11 13:13:33 +0000

    Added function to save students to CSV file

--- directory.rb
+++ directory.rb
@@ -1,28 +1,48 @@
-# student names in array
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
+@students = []
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end  
+
+def process(selection)
+  case selection
+    when "1"
+    input_students
+    when "2"
+    show_students
+    when "3"
+    save_students
+    when "9"
+    exit 
+    else
+    puts "Invalid selection"
+  end
+end
 
 def input_students
   puts "Please enter the names of the students (hit return twice to finish)"
-  students = []
   name = gets.chomp
   while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "now we have #{@students.count} students"
     name = gets.chomp
   end
-  students
+end
+
+def print_menu
+  puts "1. Input students"
+  puts "2. Display students"
+  puts "3. Save students to CSV file"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
 end
 
 def print_header
@@ -30,34 +50,26 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  students.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)" }
-end
-
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
-end
-
-def interactive_menu
-  students = []
-  loop do
-    puts "1. Input students"
-    puts "2. Display students"
-    puts "9. Exit"
-    selection = gets.chomp
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-    when "9" 
-      exit
-    else puts "Invalid selection"
-    end
+def print_student_list
+  @students.each do |student| 
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
+
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
+end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+  student_data = [student[:name], [student[:cohort]]]
+  csv_line = student_data.join(",")
+  file.puts csv_line
+end
+file.close
+end  
+
 
 
 interactive_menu
